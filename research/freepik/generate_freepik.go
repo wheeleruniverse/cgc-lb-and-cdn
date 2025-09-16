f8666e55bc4f490e36056fe176bbc826a392d2a3
Justin Wheeler <justin.wheeler@gfs.com>
2025-09-16 15:32:49 -0400

    removed unused code

--- research/freepik/generate_freepik.go
+++ research/freepik/generate_freepik.go
@@ -108,24 +108,3 @@ func main() {
 		}
 	}
 }
-
-func downloadImage(url, filename string) error {
-	resp, err := http.Get(url)
-	if err != nil {
-		return err
-	}
-	defer resp.Body.Close()
-
-	if resp.StatusCode != http.StatusOK {
-		return fmt.Errorf("failed to download image: status %d", resp.StatusCode)
-	}
-
-	file, err := os.Create(filename)
-	if err != nil {
-		return err
-	}
-	defer file.Close()
-
-	_, err = io.Copy(file, resp.Body)
-	return err
-}
